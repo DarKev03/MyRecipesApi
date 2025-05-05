242fe45bfa8e9cc192a7b81649ddab5f90587683
DarKev03 <kevingarciah03@gmail.com>
2025-05-05 13:02:24 +0200

    Estructura inicial creada

---
+++ backend/src/main/java/com/myrecipes/backend/MyrecipesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myrecipes.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrecipesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrecipesBackendApplication.class, args);
+	}
+
+}
